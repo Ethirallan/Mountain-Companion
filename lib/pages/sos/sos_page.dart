d2f2e6edcd9ce1d5a32d5971ce8155420074ae09
Benjamin Božič <ethirallan@gmail.com>
2020-06-30 23:58:45 +0200

    SOS page:  - geolocator and shoving coordinates  - call help or send sms with coordinates  - sos flashlight

--- lib/pages/sos/sos_page.dart
+++ lib/pages/sos/sos_page.dart
@@ -1,37 +1,84 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:geolocation/geolocation.dart';
 import 'package:mountaincompanion/global_widgets/mountain_app_bar.dart';
+import 'package:torch_compat/torch_compat.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class SOSPage extends StatelessWidget {
+class SOSPage extends StatefulWidget {
 
+  @override
+  _SOSPageState createState() => _SOSPageState();
+}
+
+class _SOSPageState extends State<SOSPage> {
   Future<String> getLocation() async {
-    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    return '${position.latitude}, ${position.longitude}';
-  }
-
-  Geolocator _geolocator = new Geolocator();
-
-  Future<Position> getCurrentPosition() async {
-    Position position;
-    GeolocationStatus status = await _geolocator.checkGeolocationPermissionStatus();
-
-    if (status != GeolocationStatus.disabled) {
-      position = await _geolocator.getLastKnownPosition();
-
-      if (position == null && status == GeolocationStatus.granted) {
-        position = await _geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high,
-          locationPermissionLevel: GeolocationPermission.locationWhenInUse,
-        );
-      }
+    final GeolocationResult result = await Geolocation.requestLocationPermission(
+      permission: LocationPermission(
+        android: LocationPermissionAndroid.fine,
+        ios: LocationPermissionIOS.always,
+      ),
+      openSettingsIfDenied: true,
+    );
+    if(result.isSuccessful) {
+      LocationResult result = await Geolocation.lastKnownLocation();
+      return '${result.location.latitude}, ${result.location.longitude}';
+    } else {
+      print(result.error);
+      return 'Can not get the current location at the moment!';
     }
-
-    return position;
   }
+
+  void callHelp() async {
+    const url = 'tel:112';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  void smsHelp() async {
+    String location = await getLocation();
+    String url;
+    if (Platform.isIOS) {
+      url = 'sms:&body=Need help! My location: $location';
+    } else {
+      url = 'sms:?body=Need help! My location: $location';
+    }
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  String sosProgress = ' ';
+  bool disableFlashlightButton = false;
 
   @override
   Widget build(BuildContext context) {
-
+    Future flashlightSOS (int time, String sign) async {
+      for (int i = 0; i < 3; i++) {
+        await TorchCompat.turnOn();
+        setState(() {
+          sosProgress += '$sign';
+        });
+        await Future.delayed(Duration(milliseconds: time), () async {
+          await TorchCompat.turnOff();
+        });
+        await Future.delayed(Duration(milliseconds: 800), () {
+          setState(() {
+            sosProgress += ' ';
+          });
+        });
+      }
+      setState(() {
+        sosProgress += '  ';
+      });
+    }
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -62,35 +109,82 @@ class SOSPage extends StatelessWidget {
                   width: 50,
                 ),
               ),
-              SingleChildScrollView(
+              Expanded(
                 child: Container(
-                  height: MediaQuery.of(context).size.height - 92,
+                  height: MediaQuery.of(context).size.height,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15),
+                      topLeft: Radius.circular(35),
+                      topRight: Radius.circular(35),
                     ),
                     color: Colors.white,
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      Text('My location', textAlign: TextAlign.center,),
-                      FutureBuilder(
-                        future: getCurrentPosition(),
-                        builder: (context, snapshot) {
-                          print(snapshot);
-                          if (snapshot.hasData) {
-                            return Text(snapshot.data);
-                          } else if (snapshot.hasError) {
-                            return Text('Error');
-                          } else {
-                              return Center(child: CircularProgressIndicator(),);
-                          }
-                        },
-                      ),
-                    ],
+                  child: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Text('My location', textAlign: TextAlign.center, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,),),
+                        Center(
+                          child: FutureBuilder(
+                            future: getLocation(),
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData) {
+                                return Text(snapshot.data);
+                              } else if (snapshot.hasError) {
+                                return Text('Error');
+                              } else {
+                                  return Center(child: CircularProgressIndicator(),);
+                              }
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 20, left: MediaQuery.of(context).size.width / 4, right: MediaQuery.of(context).size.width / 4),
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                            color: Colors.lightGreen,
+                            textColor: Colors.white,
+                            child: Text('Call help (112)'),
+                            onPressed: callHelp,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 20, left: MediaQuery.of(context).size.width / 4, right: MediaQuery.of(context).size.width / 4),
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                            color: Colors.lightGreen,
+                            textColor: Colors.white,
+                            child: Text('Send SOS SMS'),
+                            onPressed: smsHelp,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 20, left: MediaQuery.of(context).size.width / 4, right: MediaQuery.of(context).size.width / 4),
+                          child: RaisedButton(
+
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                            color: Colors.lightGreen,
+                            textColor: Colors.white,
+                            child: Text('SOS flashlight'),
+                            onPressed: disableFlashlightButton ? null : () async {
+                              setState(() {
+                                disableFlashlightButton = true;
+                              });
+                              await flashlightSOS(300, '•');
+                              await flashlightSOS(1500, '––');
+                              await flashlightSOS(300, '•');
+                              setState(() {
+                                sosProgress = ' ';
+                                disableFlashlightButton = false;
+                              });
+                            },
+                          ),
+                        ),
+                        Center(child: Text(sosProgress, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),),
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -99,5 +193,10 @@ class SOSPage extends StatelessWidget {
         ),
       ),
     );
+  }
+  @override
+  void dispose() {
+    TorchCompat.dispose();
+    super.dispose();
   }
 }
